bfa7f2fbfe871edf88917e79147f52d775a2b0e7
Christos <christoskaparakis95@gmail.com>
2021-04-27 10:46:00 +0200

    Entity extraction using spacy

--- script.py
+++ script.py
@@ -31,15 +31,22 @@ df["lemma"] = ""
 df["POS"] = ""
 df['sentiment_per_word'] = ""
 df['sentiment_avg'] = np.nan
+df['people'] = ""
+df['locations'] = ""
 
 # iterate through every verse (the no punctuation column)
+count1 = 0
+count2 = 0
 for i in range(len(df)):
-    doc = nlp(df.loc[i]['np'])
+    doc = nlp(df.loc[i]['t'])
     lc = []
     tok = []
     lemma = []
     pos = []
     sent = []
+    person = []
+    locations = []
+
     for token in doc:
         # print(token.text, token.pos_, token.dep_)
         lc.append(token.lower_)
@@ -48,14 +55,24 @@ for i in range(len(df)):
         pos.append(token.pos_)
         sent.append(token._.polarity)
 
+    document = nlp(df.loc[i]['t'])
+    for ent in document.ents:
+        if ent.label_ == 'PERSON' or ent.label_ == 'ORG':
+            person.append(ent.text)
+            count1 += 1
+        elif ent.label_ == 'LOC' or ent.label_ == 'GPE':
+            locations.append(ent.text)
+            count2 += 1
+
     df.at[i, 'lower'] = lc
     df.at[i, 'tokens'] = tok
     df.at[i, 'lemma'] = lemma
     df.at[i, 'POS'] = pos
     df.at[i, 'sentiment_per_word'] = sent
     df.at[i, 'sentiment_avg'] = doc._.polarity
+    df.at[i, 'people'] = person
+    df.at[i, 'locations'] = locations
 
-
-plt.hist(df['sentiment_avg'], bins=50) #histogram to check the sentiment throughout the Bible
+plt.hist(df['sentiment_avg'], bins=50)  # histogram to check the sentiment throughout the Bible
 
 df.to_csv('out.csv', index=False)
