a662963fa4c288bcf58fa6676777fea58751cee7
Christos <christoskaparakis95@gmail.com>
2021-04-27 12:26:36 +0200

    Added function for preprocessing, unigrams, bigrams and trigrams
    A lot of not important words appear very frequently though

--- script.py
+++ script.py
@@ -1,14 +1,19 @@
+import os.path
+from os import path
 import string
 import numpy as np
 import pandas as pd
 import spacy
 import matplotlib.pyplot as plt
+import seaborn as sns
+from textblob import TextBlob
+from collections import Counter, defaultdict
 from spacytextblob.spacytextblob import SpacyTextBlob
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
-path = 'data/t_kjv.csv'  # path to the bible csv
-df = pd.read_csv(path)
+path_a = 'data/t_kjv.csv'  # path to the bible csv
+df = pd.read_csv(path_a)
 df.head()
 print("number of rows(verses): " + format(df.shape[0]))
 
@@ -20,59 +25,193 @@ print("number of rows(books): " + format(df_b.shape[0]))
 nlp = spacy.load("en_core_web_sm")  # load the spacy pipeline
 nlp.add_pipe("spacytextblob")  # add this for sentiment analysis
 
-# create column without punctuations
-for i in range(len(df)):
-    df.at[i, 'np'] = df.loc[i]['t'].translate(str.maketrans('', '', string.punctuation))
 
-# creating the target columns for our dataframe
-df["lower"] = ""
-df["tokens"] = ""
-df["lemma"] = ""
-df["POS"] = ""
-df['sentiment_per_word'] = ""
-df['sentiment_avg'] = np.nan
-df['people'] = ""
-df['locations'] = ""
+def preprocess(data):
+    # creating the target columns for our dataframe
+    data["lower"] = ""
+    data["tokens"] = ""
+    data["lemma"] = ""
+    data["POS"] = ""
+    data['sentiment_per_word'] = ""
+    data['sentiment_avg'] = np.nan
+    data['people'] = ""
+    data['locations'] = ""
 
-# iterate through every verse (the no punctuation column)
-count1 = 0
-count2 = 0
-for i in range(len(df)):
-    doc = nlp(df.loc[i]['t'])
-    lc = []
-    tok = []
-    lemma = []
-    pos = []
-    sent = []
-    person = []
-    locations = []
+    # iterate through every verse (the no punctuation column)
+    count1 = 0
+    count2 = 0
+    printcounter = 0
+    for i in range(len(data)):
+        # create column without punctuations
+        data.at[i, 'np'] = data.loc[i]['t'].translate(str.maketrans('', '', string.punctuation))
+        # create column with number of words per verse
+        word_list = data.at[i, 'np'].split()
+        data.at[i, 'word_count'] = len(word_list)
 
-    for token in doc:
-        # print(token.text, token.pos_, token.dep_)
-        lc.append(token.lower_)
-        tok.append(token.text)
-        lemma.append(token.lemma_)
-        pos.append(token.pos_)
-        sent.append(token._.polarity)
+        doc = nlp(data.loc[i]['np'])  # create the lemma... etc without punctuations
+        lc = []
+        tok = []
+        lemma = []
+        pos = []
+        sent = []
+        person = []
+        locations = []
 
-    document = nlp(df.loc[i]['t'])
-    for ent in document.ents:
-        if ent.label_ == 'PERSON' or ent.label_ == 'ORG':
-            person.append(ent.text)
-            count1 += 1
-        elif ent.label_ == 'LOC' or ent.label_ == 'GPE':
-            locations.append(ent.text)
-            count2 += 1
+        for token in doc:
+            # print(token.text, token.pos_, token.dep_)
+            lc.append(token.lower_)
+            tok.append(token.text)
+            lemma.append(token.lemma_)
+            pos.append(token.pos_)
+            sent.append(token._.polarity)
 
-    df.at[i, 'lower'] = lc
-    df.at[i, 'tokens'] = tok
-    df.at[i, 'lemma'] = lemma
-    df.at[i, 'POS'] = pos
-    df.at[i, 'sentiment_per_word'] = sent
-    df.at[i, 'sentiment_avg'] = doc._.polarity
-    df.at[i, 'people'] = person
-    df.at[i, 'locations'] = locations
+        # entity extraction
+        # document = nlp(data.loc[i]['t'])
+        # for ent in document.ents:
+        #     if ent.label_ == 'PERSON' or ent.label_ == 'ORG':
+        #         person.append(ent.text)
+        #         count1 += 1
+        #     elif ent.label_ == 'LOC' or ent.label_ == 'GPE':
+        #         locations.append(ent.text)
+        #         count2 += 1
 
-plt.hist(df['sentiment_avg'], bins=50)  # histogram to check the sentiment throughout the Bible
+        data.at[i, 'lower'] = lc
+        data.at[i, 'tokens'] = tok
+        data.at[i, 'lemma'] = lemma
+        data.at[i, 'POS'] = pos
+        data.at[i, 'sentiment_per_word'] = sent
+        data.at[i, 'sentiment_avg'] = doc._.polarity
+        # data.at[i, 'people'] = person
+        # data.at[i, 'locations'] = locations
 
-df.to_csv('out.csv', index=False)
+        if printcounter % 1000 == 0:
+            print('Progress report: ', round(printcounter / len(data) * 100), '% of verses processed.')
+        printcounter += 1
+
+    data.to_csv('out.csv', index=False)
+
+
+def unigrams(data):
+    # UNIGRAMS
+    NT_unigrams = defaultdict(int)  # New Testament
+    OT_unigrams = defaultdict(int)  # Old Testament
+
+    for x in range(len(data)):
+        book = data.iloc[x]['b']
+        testament = df_b[df_b['b'] == book].iloc[0]['t']
+        if testament == 'OT':
+            for word in data.loc[x]['lemma']:
+                OT_unigrams[word.lower()] += 1  # use lowercase words
+        elif testament == 'NT':
+            for word in data.loc[x]['lemma']:
+                NT_unigrams[word.lower()] += 1
+
+    df_NT_unigrams = pd.DataFrame(sorted(NT_unigrams.items(), key=lambda x: x[1])[::-1])
+    df_OT_unigrams = pd.DataFrame(sorted(OT_unigrams.items(), key=lambda x: x[1])[::-1])
+
+    fig, axes = plt.subplots(ncols=2, figsize=(12, 10), dpi=80)
+    plt.tight_layout()
+
+    N = 25
+
+    sns.barplot(y=df_NT_unigrams[0].values[:N], x=df_NT_unigrams[1].values[:N], ax=axes[0], color='red')
+    sns.barplot(y=df_OT_unigrams[0].values[:N], x=df_OT_unigrams[1].values[:N], ax=axes[1], color='green')
+
+    for i in range(2):
+        axes[i].spines['right'].set_visible(False)
+        axes[i].set_xlabel('')
+        axes[i].set_ylabel('')
+        axes[i].tick_params(axis='x', labelsize=10)
+        axes[i].tick_params(axis='y', labelsize=10)
+
+    axes[0].set_title(f'Top {N} most common unigrams in New Testament', fontsize=10)
+    axes[1].set_title(f'Top {N} most common unigrams in Old Testament', fontsize=10)
+
+    plt.show()
+
+
+def bigrams(data):
+    NT_bigrams = defaultdict(int)  # New Testament
+    OT_bigrams = defaultdict(int)  # Old Testament
+
+    for x in range(len(data)):
+        book = data.iloc[x]['b']
+        testament = df_b[df_b['b'] == book].iloc[0]['t']
+        blob = TextBlob(' '.join(data.iloc[x]['lower']))
+        if testament == 'OT':
+            for bigram in blob.ngrams(2):
+                OT_bigrams[' '.join(bigram)] += 1  # use lowercase words
+        elif testament == 'NT':
+            for bigram in blob.ngrams(2):
+                NT_bigrams[' '.join(bigram)] += 1  # use lowercase words
+
+    df_NT_bigrams = pd.DataFrame(sorted(NT_bigrams.items(), key=lambda x: x[1])[::-1])
+    df_OT_bigrams = pd.DataFrame(sorted(OT_bigrams.items(), key=lambda x: x[1])[::-1])
+
+    fig, axes = plt.subplots(ncols=2, figsize=(12, 10), dpi=80)
+    plt.tight_layout()
+
+    N = 25
+
+    sns.barplot(y=df_NT_bigrams[0].values[:N], x=df_NT_bigrams[1].values[:N], ax=axes[0], color='red')
+    sns.barplot(y=df_OT_bigrams[0].values[:N], x=df_OT_bigrams[1].values[:N], ax=axes[1], color='green')
+
+    for i in range(2):
+        axes[i].spines['right'].set_visible(False)
+        axes[i].set_xlabel('')
+        axes[i].set_ylabel('')
+        axes[i].tick_params(axis='x', labelsize=10)
+        axes[i].tick_params(axis='y', labelsize=10)
+
+    axes[0].set_title(f'Top {N} most common bigrams in New Testament', fontsize=10)
+    axes[1].set_title(f'Top {N} most common bigrams in Old Testament', fontsize=10)
+
+    plt.show()
+
+
+def trigrams(data):
+    NT_trigrams = defaultdict(int)  # New Testament
+    OT_trigrams = defaultdict(int)  # Old Testament
+
+    for x in range(len(data)):
+        book = data.iloc[x]['b']
+        testament = df_b[df_b['b'] == book].iloc[0]['t']
+        blob = TextBlob(' '.join(data.iloc[x]['lower']))
+        if testament == 'OT':
+            for trigram in blob.ngrams(3):
+                OT_trigrams[' '.join(trigram)] += 1  # use lowercase words
+        elif testament == 'NT':
+            for trigram in blob.ngrams(3):
+                NT_trigrams[' '.join(trigram)] += 1  # use lowercase words
+
+    df_NT_trigrams = pd.DataFrame(sorted(NT_trigrams.items(), key=lambda x: x[1])[::-1])
+    df_OT_trigrams = pd.DataFrame(sorted(OT_trigrams.items(), key=lambda x: x[1])[::-1])
+
+    fig, axes = plt.subplots(ncols=2, figsize=(12, 10), dpi=80)
+    plt.tight_layout()
+
+    N = 25
+
+    sns.barplot(y=df_NT_trigrams[0].values[:N], x=df_NT_trigrams[1].values[:N], ax=axes[0], color='red')
+    sns.barplot(y=df_OT_trigrams[0].values[:N], x=df_OT_trigrams[1].values[:N], ax=axes[1], color='green')
+
+    for i in range(2):
+        axes[i].spines['right'].set_visible(False)
+        axes[i].set_xlabel('')
+        axes[i].set_ylabel('')
+        axes[i].tick_params(axis='x', labelsize=10)
+        axes[i].tick_params(axis='y', labelsize=10)
+
+    axes[0].set_title(f'Top {N} most common trigrams in New Testament', fontsize=10)
+    axes[1].set_title(f'Top {N} most common trigrams in Old Testament', fontsize=10)
+
+    plt.show()
+
+
+if path.exists("out.csv"):
+    df = pd.read_csv("out.csv")
+else:
+    preprocess(df)
+
+
+# plt.hist(df['sentiment_avg'], bins=50)  # histogram to check the sentiment throughout the Bible
