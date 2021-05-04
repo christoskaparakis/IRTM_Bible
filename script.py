f07a46d8ab921aea637ea17dd3b17cb140c36717
Christos <christoskaparakis95@gmail.com>
2021-05-04 21:31:50 +0200

    Changed output filetype to pickle

--- script.py
+++ script.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from textblob import TextBlob
 from collections import Counter, defaultdict
+import en_core_web_sm
+from tabulate import tabulate
 from spacytextblob.spacytextblob import SpacyTextBlob
 
 pd.options.mode.chained_assignment = None  # default='warn'
@@ -22,20 +24,20 @@ df_b = pd.read_csv(path_b)
 df_b.head()
 print("number of rows(books): " + format(df_b.shape[0]))
 
-nlp = spacy.load("en_core_web_sm")  # load the spacy pipeline
+nlp = en_core_web_sm.load()  # load the spacy pipeline
 nlp.add_pipe("spacytextblob")  # add this for sentiment analysis
 
 
 def preprocess(data):
     # creating the target columns for our dataframe
-    data["lower"] = ""
-    data["tokens"] = ""
-    data["lemma"] = ""
-    data["POS"] = ""
-    data['sentiment_per_word'] = ""
+    data["lower"] = np.empty((len(data), 0)).tolist()
+    data["tokens"] = np.empty((len(data), 0)).tolist()
+    data["lemma"] = np.empty((len(data), 0)).tolist()
+    data["POS"] = np.empty((len(data), 0)).tolist()
+    data['sentiment_per_word'] = np.empty((len(data), 0)).tolist()
     data['sentiment_avg'] = np.nan
-    data['people'] = ""
-    data['locations'] = ""
+    data['people'] = np.empty((len(data), 0)).tolist()
+    data['locations'] = np.empty((len(data), 0)).tolist()
 
     # iterate through every verse (the no punctuation column)
     count1 = 0
@@ -66,14 +68,14 @@ def preprocess(data):
             sent.append(token._.polarity)
 
         # entity extraction
-        # document = nlp(data.loc[i]['t'])
-        # for ent in document.ents:
-        #     if ent.label_ == 'PERSON' or ent.label_ == 'ORG':
-        #         person.append(ent.text)
-        #         count1 += 1
-        #     elif ent.label_ == 'LOC' or ent.label_ == 'GPE':
-        #         locations.append(ent.text)
-        #         count2 += 1
+        document = nlp(data.loc[i]['t'])
+        for ent in document.ents:
+            if ent.label_ == 'PERSON' or ent.label_ == 'ORG':
+                person.append(ent.text)
+                count1 += 1
+            elif ent.label_ == 'LOC' or ent.label_ == 'GPE':
+                locations.append(ent.text)
+                count2 += 1
 
         data.at[i, 'lower'] = lc
         data.at[i, 'tokens'] = tok
@@ -81,17 +83,19 @@ def preprocess(data):
         data.at[i, 'POS'] = pos
         data.at[i, 'sentiment_per_word'] = sent
         data.at[i, 'sentiment_avg'] = doc._.polarity
-        # data.at[i, 'people'] = person
-        # data.at[i, 'locations'] = locations
+        data.at[i, 'people'] = person
+        data.at[i, 'locations'] = locations
 
         if printcounter % 1000 == 0:
             print('Progress report: ', round(printcounter / len(data) * 100), '% of verses processed.')
         printcounter += 1
 
     data.to_csv('out.csv', index=False)
+    data.to_pickle('out.pkl')
+    return data
 
 
-def unigrams(data):
+def unigrams(data, plot=True):
     # UNIGRAMS
     NT_unigrams = defaultdict(int)  # New Testament
     OT_unigrams = defaultdict(int)  # Old Testament
@@ -109,25 +113,26 @@ def unigrams(data):
     df_NT_unigrams = pd.DataFrame(sorted(NT_unigrams.items(), key=lambda x: x[1])[::-1])
     df_OT_unigrams = pd.DataFrame(sorted(OT_unigrams.items(), key=lambda x: x[1])[::-1])
 
-    fig, axes = plt.subplots(ncols=2, figsize=(12, 10), dpi=80)
-    plt.tight_layout()
+    if plot:
+        fig, axes = plt.subplots(ncols=2, figsize=(12, 10), dpi=80)
+        plt.tight_layout()
 
-    N = 25
+        N = 25
 
-    sns.barplot(y=df_NT_unigrams[0].values[:N], x=df_NT_unigrams[1].values[:N], ax=axes[0], color='red')
-    sns.barplot(y=df_OT_unigrams[0].values[:N], x=df_OT_unigrams[1].values[:N], ax=axes[1], color='green')
+        sns.barplot(y=df_NT_unigrams[0].values[:N], x=df_NT_unigrams[1].values[:N], ax=axes[0], color='red')
+        sns.barplot(y=df_OT_unigrams[0].values[:N], x=df_OT_unigrams[1].values[:N], ax=axes[1], color='green')
 
-    for i in range(2):
-        axes[i].spines['right'].set_visible(False)
-        axes[i].set_xlabel('')
-        axes[i].set_ylabel('')
-        axes[i].tick_params(axis='x', labelsize=10)
-        axes[i].tick_params(axis='y', labelsize=10)
+        for i in range(2):
+            axes[i].spines['right'].set_visible(False)
+            axes[i].set_xlabel('')
+            axes[i].set_ylabel('')
+            axes[i].tick_params(axis='x', labelsize=10)
+            axes[i].tick_params(axis='y', labelsize=10)
 
-    axes[0].set_title(f'Top {N} most common unigrams in New Testament', fontsize=10)
-    axes[1].set_title(f'Top {N} most common unigrams in Old Testament', fontsize=10)
+        axes[0].set_title(f'Top {N} most common unigrams in New Testament', fontsize=10)
+        axes[1].set_title(f'Top {N} most common unigrams in Old Testament', fontsize=10)
 
-    plt.show()
+        plt.show()
 
 
 def bigrams(data):
@@ -209,9 +214,9 @@ def trigrams(data):
 
 
 if path.exists("out.csv"):
-    df = pd.read_csv("out.csv")
+    df = pd.read_pickle("out.pkl")
 else:
-    preprocess(df)
+    df = preprocess(df)
 
 
 # plt.hist(df['sentiment_avg'], bins=50)  # histogram to check the sentiment throughout the Bible
